9a0a54b1db803c2b7af3b4b857fca0667ca47c01
anouarboualiou <anwarboualiou@gmail.com>
2026-05-20 23:08:52 +0200

    Fix bd

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,29 +1,37 @@
-//Creamos el archivo de conexion a la base de datos
+require('dotenv').config()
 
-require('dotenv').config();
 const mysql = require('mysql2')
 
-
 const link = mysql.createConnection({
-    
+
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    dateStrings: true
 
-});
+    dateStrings: true,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
 link.connect((error) => {
 
     if (error) {
-        console.error('Error de conexión MySql: ', error)
+
+        console.error(
+            'Error de conexión MySql:',
+            error
+        )
     }
     else {
-        console.log('MySql conectado con éxito')
+
+        console.log(
+            'MySql conectado con éxito'
+        )
     }
 })
-
 
 module.exports = link
